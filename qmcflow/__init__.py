614ff69754376e3dea29c66ad945fcd6b5eec1b3
Luuk-Renden <129054865+Luuk-Renden@users.noreply.github.com>
2023-04-14 13:20:39 +0200

    Added code generated by cookiecutter

---
+++ qmcflow/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about qmcflow"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Luuk Renden"
+__email__ = "l.renden@esciencecenter.nl"
+__version__ = "0.1.0"
